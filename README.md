39cefb11562c97cdcbc3a57802a55a07f8767c6f
larsupb <larsupb@gmx.net>
2024-01-03 12:01:45 +0100

    bugfixes and smaller improvements

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1>Simple Image Tagger</h1>
+<p>
+A simple image tagger which allows a lean process for tagging images. 
+Tags are saved in .txt format whose filename corresponds to the image name. This app is powered by gradio.
+</p>
+
+<img width="512" src="./examples/example01.png">
+
+<h2>Installation</h2>
+**Clone repository** 
+```
+git clone https://github.com/larsupb/ImageTagger
+```
+**Navigate into installation directory and create venv**
+```
+cd ImageTagger
+python -m venv venv
+```
+**Activate and setup venv**
+```
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+<h2>Launch instructions</h2>
+Open a console and navigate to installation directory. 
+Run app by executing: 
+```
+python main.py
+```
+
--- main.py
+++ main.py
@@ -45,63 +45,58 @@ def read_caption(caption_path):
     return caption_text
 
 
-css = ""
-if os.path.exists("./style.css"):
-    with open(os.path.join("./style.css"), "r", encoding="utf8") as file:
-        css = file.read() + "\n"
+def save_caption(new_text, old_value):
+    global dataset
 
-with gr.Blocks(css=css) as app:
-    with gr.Row():
-        button_open_dir = gr.Button(value=folder_symbol, elem_id='open_folder_small')
-        input_folder_path = gr.Textbox(value="", placeholder="Directory containing the images")
-        checkbox_only_missing_captions = gr.Checkbox(label="Only images missing captions", value=False)
-        button_open_dataset = gr.Button(value=process_symbol + " Open")
-
-    with gr.Row():
-        slider = gr.Slider(minimum=-1, maximum=len(dataset.images) - 1, value=-1, label="Image Index", step=1)
-        slider_selected_value = gr.Number(visible=False)
-        button_backward = gr.Button(value=backward_symbol, elem_id='open_folder_small')
-        button_forward = gr.Button(value=forward_symbol, elem_id='open_folder_small')
-    with gr.Row():
-        with gr.Column():
-            gr.Markdown("Image")
-            image_path = gr.Textbox(interactive=False, label="Path")
-            image_display = gr.Image()
-        with gr.Column():
-            gr.Markdown("Caption text")
-            caption_display = gr.Textbox(placeholder="Caption")
+    caption_path = dataset.caption_paths[old_value]
+    if caption_path not in dataset.caption_texts:
+        return
+    if dataset.caption_texts[caption_path] != new_text:
+        with open(caption_path, 'w') as f:
+            f.write(new_text)
 
 
-    def save_caption(new_text, old_value):
-        global dataset
+if __name__ == '__main__':
 
-        caption_path = dataset.caption_paths[old_value]
-        if caption_path not in dataset.caption_texts:
-            return
-        if dataset.caption_texts[caption_path] != new_text:
-            with open(caption_path, 'w') as f:
-                f.write(new_text)
+    css = ""
+    if os.path.exists("./style.css"):
+        with open(os.path.join("./style.css"), "r", encoding="utf8") as file:
+            css = file.read() + "\n"
 
+    with gr.Blocks(css=css) as app:
+        with gr.Row():
+            button_open_dir = gr.Button(value=folder_symbol, elem_id='open_folder_small')
+            input_folder_path = gr.Textbox(value="", placeholder="Directory containing the images")
+            checkbox_only_missing_captions = gr.Checkbox(label="Only images missing captions", value=False)
+            button_open_dataset = gr.Button(value=process_symbol + " Open")
 
-    button_open_dir.click(get_folder_path, inputs=input_folder_path, outputs=input_folder_path)
-    button_open_dataset.click(init_dataset, inputs=[input_folder_path, checkbox_only_missing_captions], outputs=slider)
+        with gr.Row():
+            slider = gr.Slider(minimum=-1, maximum=len(dataset.images) - 1, value=-1, label="Image Index", step=1)
+            slider_selected_value = gr.Number(visible=False)
+            button_backward = gr.Button(value=backward_symbol, elem_id='open_folder_small')
+            button_forward = gr.Button(value=forward_symbol, elem_id='open_folder_small')
+        with gr.Row():
+            with gr.Column():
+                gr.Markdown("Image")
+                image_path = gr.Textbox(interactive=False, label="Path")
+                image_display = gr.Image()
+            with gr.Column():
+                gr.Markdown("Caption text")
+                caption_display = gr.Textbox(placeholder="Caption")
 
-    slider.change(save_caption, inputs=[caption_display, slider_selected_value], outputs=None)
-    slider.change(load_index, inputs=slider,
-                  outputs=[image_path, image_display, caption_display, slider_selected_value])
+        button_open_dir.click(get_folder_path, inputs=input_folder_path, outputs=input_folder_path)
+        button_open_dataset.click(init_dataset, inputs=[input_folder_path, checkbox_only_missing_captions], outputs=slider)
 
+        slider.change(save_caption, inputs=[caption_display, slider_selected_value], outputs=None)
+        slider.change(load_index, inputs=slider,
+                      outputs=[image_path, image_display, caption_display, slider_selected_value])
 
-    def navigate_backward(current_index) -> int:
-        return max(current_index - 1, 0)
+        def navigate_backward(current_index) -> int:
+            return max(current_index - 1, 0)
+        button_backward.click(navigate_backward, inputs=slider, outputs=slider)
 
+        def navigate_forward(current_index) -> int:
+            return min(current_index + 1, dataset.len())
+        button_forward.click(navigate_forward, inputs=slider, outputs=slider)
 
-    button_backward.click(navigate_backward, inputs=slider, outputs=slider)
-
-
-    def navigate_forward(current_index) -> int:
-        return min(current_index + 1, dataset.len())
-
-
-    button_forward.click(navigate_forward, inputs=slider, outputs=slider)
-
-app.launch()
+    app.launch()
